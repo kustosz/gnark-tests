c07af2499983dea6fa62c35f1128a72330b92b37
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-08-14 18:14:24 +0200

    attempt point compression

--- solidity/solidity_groth16_test.go
+++ solidity/solidity_groth16_test.go
@@ -2,6 +2,9 @@ package solidity
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fp"
 	"github.com/consensys/gnark/examples/exponentiate"
 	"math/big"
 	"os"
@@ -110,8 +113,9 @@ func (t *ExportSolidityTestSuiteGroth16) TestVerifyProof() {
 
 	// solidity contract inputs
 	var (
-		proofEvm [8]*big.Int
-		input    [2]*big.Int
+		proofEvm           [8]*big.Int
+		proofCompressedEvm [4]*big.Int
+		input              [2]*big.Int
 	)
 
 	// proof.Ar, proof.Bs, proof.Krs
@@ -124,12 +128,33 @@ func (t *ExportSolidityTestSuiteGroth16) TestVerifyProof() {
 	proofEvm[6] = new(big.Int).SetBytes(proofBytes[fpSize*6 : fpSize*7])
 	proofEvm[7] = new(big.Int).SetBytes(proofBytes[fpSize*7 : fpSize*8])
 
+	g1 := new(bn254.G1Affine)
+	g2 := new(bn254.G2Affine)
+	g1.SetBytes(proofBytes[fpSize*0 : fpSize*2])
+	proofCompressedEvm[0] = compressG1ForContract(g1)
+	g2.SetBytes(proofBytes[fpSize*2 : fpSize*6])
+	proofCompressedEvm[1], proofCompressedEvm[2] = compressG2ForContract(g2)
+	g1.SetBytes(proofBytes[fpSize*6 : fpSize*8])
+	proofCompressedEvm[3] = compressG1ForContract(g1)
+	fmt.Println("PROOF COMP")
+	for _, v := range proofCompressedEvm {
+		shf := new(big.Int).Set(v)
+		fmt.Println(shf.Text(16))
+	}
+	fmt.Println("PROOF")
+	for _, v := range proofEvm {
+		fmt.Println(v.Text(16))
+	}
+
 	// public witness
 	input[0] = new(big.Int).SetUint64(3)
 	input[1] = new(big.Int).SetUint64(81)
 
 	// call the contract
 	err = t.verifierContract.VerifyProof(&bind.CallOpts{}, proofEvm, input)
+	t.NoError(err, "calling verifier on chain gave error")
+
+	err = t.verifierContract.VerifyCompressedProof(&bind.CallOpts{}, proofCompressedEvm, input)
 	t.NoError(err, "calling verifier on chain gave error")
 
 	// (wrong) public witness
@@ -139,4 +164,75 @@ func (t *ExportSolidityTestSuiteGroth16) TestVerifyProof() {
 	err = t.verifierContract.VerifyProof(&bind.CallOpts{}, proofEvm, input)
 	t.Error(err, "calling verifier on chain succeeded, and shouldn't have")
 
+}
+
+func compressG1ForContract(g1 *bn254.G1Affine) *big.Int {
+	y := new(fp.Element)
+	y.Exp(g1.X, big.NewInt(3))
+	y.Add(y, new(fp.Element).SetUint64(3))
+	y.Sqrt(y)
+	res := new(big.Int)
+	g1.X.BigInt(res)
+	res.Lsh(res, 1)
+	if !y.Equal(&g1.Y) {
+		res.Or(res, big.NewInt(1))
+	}
+	return res
+}
+
+func compressG2ForContract(g2 *bn254.G2Affine) (*big.Int, *big.Int) {
+	a0 := new(fp.Element).SetUint64(27)
+	a0.Div(a0, new(fp.Element).SetUint64(82))
+	a0.Add(a0, new(fp.Element).Exp(g2.X.A0, new(big.Int).SetUint64(3)))
+	tmp := new(fp.Element)
+	tmp.Mul(new(fp.Element).SetUint64(3), &g2.X.A0)
+	tmp.Mul(tmp, &g2.X.A1)
+	tmp.Mul(tmp, &g2.X.A1)
+	a0.Sub(a0, tmp)
+	a0sq := new(fp.Element).Square(a0)
+
+	a1 := new(fp.Element).SetUint64(3)
+	a1.Div(a1, new(fp.Element).SetUint64(82))
+	a1.Add(a1, new(fp.Element).Exp(g2.X.A1, new(big.Int).SetUint64(3)))
+	tmp.Mul(new(fp.Element).SetUint64(3), &g2.X.A0)
+	tmp.Mul(tmp, &g2.X.A0)
+	tmp.Mul(tmp, &g2.X.A1)
+	a1.Sub(a1, tmp)
+	a1.Neg(a1)
+	a1sq := new(fp.Element).Square(a1)
+
+	d := new(fp.Element)
+	d.Add(a0sq, a1sq)
+	d.Sqrt(d)
+
+	// Trial and error find the signs.
+	for bits := 0; bits < 4; bits++ {
+		dc := new(fp.Element).Set(d)
+		if bits&1 != 0 {
+			dc.Neg(dc)
+		}
+		b0 := new(fp.Element)
+		b0.Add(a0, dc)
+		b0.Div(b0, new(fp.Element).SetUint64(2))
+		b0.Sqrt(b0)
+		b1 := new(fp.Element)
+		b1.Mul(b0, new(fp.Element).SetUint64(2))
+		b1.Inverse(b1)
+		b1.Mul(b1, a1)
+		if bits&2 != 0 {
+			b0.Neg(b0)
+			b1.Neg(b1)
+		}
+		if b0.Equal(&g2.Y.A0) && b1.Equal(&g2.Y.A1) {
+			r0 := new(big.Int)
+			g2.X.A0.BigInt(r0)
+			r0.Lsh(r0, 2)
+			r0.Or(r0, big.NewInt(int64(bits)))
+			r1 := new(big.Int)
+			g2.X.A1.BigInt(r1)
+
+			return r0, r1
+		}
+	}
+	panic("impossible: no solution")
 }
